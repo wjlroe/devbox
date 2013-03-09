fc7392388c85b621dde7e90b064521e652b197e8
William Roe <willroe@gmail.com>
2013-03-09 00:59:32 +0000

    Initial berkshelf bootstrap

---
+++ README.md
@@ -0,0 +1,15 @@
+# devbox cookbook
+
+MASSIVE wrapper cookbook for everything I feel the discerning developer needs
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: William Roe (<code@wjlr.org.uk>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "devbox"
+maintainer       "YOUR_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures devbox"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
