ab9e61547a1deb9fffe5ee95cb473ac43f701400
William Roe <willroe@gmail.com>
2013-03-09 02:07:21 +0000

    Move apt dependency to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             "devbox"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "William Roe"
+maintainer_email "chef@wjlr.org.uk"
 license          "All rights reserved"
 description      "Installs/Configures devbox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+depends 'apt', '~> 1.9'
